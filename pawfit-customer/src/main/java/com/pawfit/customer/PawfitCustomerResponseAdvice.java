85836753de72650cb1a28e1924d0d4c30fbdcb8c
weejinyoung <a8118199@gmail.com>
2024-11-16 13:22:31 +0900

    feat: add h2

--- pawfit-customer/src/main/java/com/pawfit/customer/PawfitCustomerResponseAdvice.java
+++ pawfit-customer/src/main/java/com/pawfit/customer/PawfitCustomerResponseAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
 @ControllerAdvice(
         value = {
                 "com.pawfit.customer.presentation"
