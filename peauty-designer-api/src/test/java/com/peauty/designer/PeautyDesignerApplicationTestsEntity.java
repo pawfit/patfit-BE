1cec54ffb6eebb41710656095a85a183d6e6f776
weejinyoung <a8118199@gmail.com>
2024-11-21 07:53:54 +0900

    feat: designer module

--- peauty-designer-api/src/test/java/com/peauty/designer/PeautyDesignerApplicationTestsEntity.java
+++ peauty-designer-api/src/test/java/com/peauty/designer/PeautyDesignerApplicationTestsEntity.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PeautyDesignerApplicationTests {
+class PeautyDesignerApplicationTestsEntity {
 
 	@Test
 	void contextLoads() {
