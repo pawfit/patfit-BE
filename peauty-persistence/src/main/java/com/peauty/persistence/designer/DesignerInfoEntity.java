46a7baaee48e001b475fa9d7e097cbcc2d7adcc0
Shanate <redeyes5240@gmail.com>
2024-11-27 15:50:14 +0900

    feat: Create Entity - DesignerInfo, Shop

---
+++ peauty-persistence/src/main/java/com/peauty/persistence/designer/DesignerInfoEntity.java
@@ -0,0 +1,36 @@
+package com.peauty.persistence.designer;
+
+import com.peauty.persistence.config.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "designer_info")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class DesignerInfoEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "designer_id")
+    private DesignerEntity designer;
+
+    @Column(name = "introduce", length = 50)
+    private String introduce;
+
+    @Column(name = "notice", length = 100)
+    private String notice;
+
+    @Lob
+    @Column(name = "banner_image_url")
+    private String bannerImageUrl;
+
+    @Column(name = "career")
+    private Integer career;
+
+}
---
+++ peauty-persistence/src/main/java/com/peauty/persistence/designer/ShopEntity.java
@@ -0,0 +1,35 @@
+package com.peauty.persistence.designer;
+
+import com.peauty.persistence.config.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "shop")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ShopEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "designer_id")
+    private DesignerEntity designer;
+
+    @Column(name = "shop_name", length = 30, nullable = false)
+    private String shopName;
+
+    @Column(name = "direction_guide", length = 50, nullable = false)
+    private String directionGuide;
+
+    @Column(name = "open_hours", length = 30, nullable = false)
+    private String openHours;
+
+    @Column(name = "payment_options", length = 30, nullable = false)
+    private String paymentOptions;
+
+}
