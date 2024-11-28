b76dd7929bf85863b870976ae338d254d79e4949
weejinyoung <a8118199@gmail.com>
2024-11-28 11:24:17 +0900

    feat: swagger config profile change

--- peauty-designer-api/src/main/java/com/peauty/designer/presentation/config/SwaggerConfig.java
+++ peauty-designer-api/src/main/java/com/peauty/designer/presentation/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"local", "beta"})
+@Profile({"designer-local", "designer-dev"})
 public class SwaggerConfig {
 
     @Bean
