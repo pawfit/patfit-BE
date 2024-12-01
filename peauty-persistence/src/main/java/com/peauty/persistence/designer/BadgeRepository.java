f61958296ef4d82f0d3b06e707a0625e4c173c74
seokjin <tjrwls08088@naver.com>
2024-12-02 04:14:12 +0900

    style: 사용하지 않는 import, 파일 삭제

--- peauty-persistence/src/main/java/com/peauty/persistence/designer/BadgeRepository.java
+++ peauty-persistence/src/main/java/com/peauty/persistence/designer/BadgeRepository.java
@@ -1,6 +1,5 @@
 package com.peauty.persistence.designer;
 
-import com.peauty.domain.designer.Badge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
