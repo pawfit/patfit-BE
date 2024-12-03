1fcdf794af6734925cffe1fc3aafc34a41920a38
weejinyoung <a8118199@gmail.com>
2024-12-03 22:37:32 +0900

    feat: impl bidding repository and entity

---
+++ peauty-persistence/src/main/java/com/peauty/persistence/bidding/BiddingThreadEntity.java
@@ -0,0 +1,45 @@
+package com.peauty.persistence.bidding;
+
+import com.peauty.domain.bidding.BiddingThreadStatus;
+import com.peauty.domain.bidding.BiddingThreadStep;
+import com.peauty.domain.bidding.BiddingThreadTimeInfo;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bidding_thread")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class BiddingThreadEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "bidding_process_id", nullable = false)
+    private Long biddingProcessId;
+
+    @Column(name = "designer_id", nullable = false)
+    private Long designerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "step", nullable = false)
+    private BiddingThreadStep step;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private BiddingThreadStatus status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "step_modified_at", nullable = false)
+    private LocalDateTime stepModifiedAt;
+
+    @Column(name = "status_modified_at", nullable = false)
+    private LocalDateTime statusModifiedAt;
+}
