7fe507404efb39498815dc94cbd591d41414976a
Shanate <redeyes5240@gmail.com>
2024-12-04 18:34:34 +0900

    feat: Query - Searching Workspace Data Data Matching the Specific Condition

--- peauty-persistence/src/main/java/com/peauty/persistence/designer/WorkspaceRepository.java
+++ peauty-persistence/src/main/java/com/peauty/persistence/designer/WorkspaceRepository.java
@@ -1,10 +1,21 @@
 package com.peauty.persistence.designer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface
 WorkspaceRepository extends JpaRepository<WorkspaceEntity, Long> {
     WorkspaceEntity findByDesignerId(Long userId);
+    Optional<WorkspaceEntity> findAllByDesignerId(Long designerId);
+    List<WorkspaceEntity> findByAddress(String address);
+
+    @Query("SELECT w FROM WorkspaceEntity w WHERE w.address LIKE CONCAT(:baseAddress, '%')")
+    List<WorkspaceEntity> findByBaseAddress(@Param("baseAddress") String baseAddress);
+
 }
