03e208415c7f99a20250aa628739e93e556eac4d
Shanate <redeyes5240@gmail.com>
2024-12-05 22:19:55 +0900

    refactor: Remove Unnecessary Query

--- peauty-persistence/src/main/java/com/peauty/persistence/designer/BadgeRepository.java
+++ peauty-persistence/src/main/java/com/peauty/persistence/designer/BadgeRepository.java
@@ -1,18 +1,12 @@
 package com.peauty.persistence.designer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BadgeRepository extends JpaRepository<BadgeEntity, Long> {
-
-    @Query("SELECT b FROM BadgeEntity b WHERE b.id = :badgeId")
-    Optional<BadgeEntity> findBadgeById(@Param("badgeId") Long badgeId);
 
     Optional<BadgeEntity> findById(Long id);
 }
