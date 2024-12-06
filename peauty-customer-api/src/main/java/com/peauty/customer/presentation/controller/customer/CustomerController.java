776982a3eb0e5fe97fef6986c4bc5e2c2d6408f3
Shanate <redeyes5240@gmail.com>
2024-12-06 17:08:33 +0900

    fix: Allow RESTful API

--- peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/customer/CustomerController.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/customer/CustomerController.java
@@ -15,28 +15,28 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @Tag(name = "Customer", description = "Customer API")
-@RequestMapping("/v1/users")
+@RequestMapping("/v1")
 @RequiredArgsConstructor
 public class CustomerController {
 
     private final CustomerService customerService;
 //    private final DesignerService designerService;
 
-    @PostMapping(value = "/{userId}/profile/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/users/{userId}/profile/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "고객 프로필 이미지 업로드", description = "고객의 프로필 이미지 업로드 API 진입점입니다.")
     public UploadProfileImageResponse uploadProfileImage(@PathVariable Long userId, @RequestParam("image") MultipartFile image) {
         UploadProfileImageResult result = customerService.uploadProfileImage(userId, image);
         return UploadProfileImageResponse.from(result);
     }
 
-    @GetMapping("/{userId}/profile")
+    @GetMapping("/users/{userId}/profile")
     @Operation(summary = "고객 프로필 조회", description = "고객의 프로필 조회 API 진입점입니다.")
     public GetCustomerProfileResponse getCustomerProfile(@PathVariable Long userId) {
         GetCustomerProfileResult result = customerService.getCustomerProfile(userId);
         return GetCustomerProfileResponse.from(result);
     }
 
-    @PutMapping("/{userId}/profile")
+    @PutMapping("/users/{userId}/profile")
     @Operation(summary = "고객 프로필 수정", description = "고객의 프로필 수정 API 진입점입니다.")
     public UpdateCustomerProfileResponse updateCustomerProfile(
             @PathVariable Long userId,
@@ -46,21 +46,21 @@ public class CustomerController {
         return UpdateCustomerProfileResponse.from(result);
     }
 
-    @GetMapping("/check")
+    @GetMapping("/users/check")
     @Operation(summary = "고객 닉네임 중복 체크", description = "고객의 닉네임 중복 체크 API 진입점입니다.")
     public CheckCustomerNicknameDuplicatedResponse checkCustomerNicknameDuplicated(@RequestParam String nickname) {
         customerService.checkCustomerNicknameDuplicated(nickname);
         return new CheckCustomerNicknameDuplicatedResponse("사용해도 좋은 닉네임입니다.");
     }
 
-    @GetMapping("/{userId}/serach")
+    @GetMapping("/users/{userId}/serach")
     @Operation(summary = "주변 디자이너 매장 조회", description = "고객 주소와 같은 디자이너의 매장을 조회하는 API 진입점입니다.")
     public GetAroundWorkspacesResponse getAroundWorkspaces(@PathVariable Long userId) {
         GetAroundWorkspacesResult result = customerService.getAroundWorkspaces(userId);
         return GetAroundWorkspacesResponse.from(result);
     }
 
-    @GetMapping("/search/{designerId}/badges")
+    @GetMapping("/designers/{designerId}/badges")
     @Operation(summary = "고객 관점 디자이너 뱃지 조회", description = "고객 관점에서 디자이너의 획득한 뱃지를 조회하는 API 진입점입니다.")
     public GetDesignerBadgesForCustomerResponse getDesignerBadgesForCustomer(@PathVariable Long designerId) {
         GetDesignerBadgesForCustomerResult result = customerService.getDesignerBadgesByCustomer(designerId);
