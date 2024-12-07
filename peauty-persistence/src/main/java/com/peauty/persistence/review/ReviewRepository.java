a8f222273fad78f853080e88f083540cbd7d7f19
Shanate <redeyes5240@gmail.com>
2024-12-07 17:40:57 +0900

    refactor: Wrong Code Refactor

--- peauty-persistence/src/main/java/com/peauty/persistence/review/ReviewRepository.java
+++ peauty-persistence/src/main/java/com/peauty/persistence/review/ReviewRepository.java
@@ -1,8 +1,8 @@
 package com.peauty.persistence.review;
 
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends JpaAttributeConverter<ReviewEntity, Long> {
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
 }
