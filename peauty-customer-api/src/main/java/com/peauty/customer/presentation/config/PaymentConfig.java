9c7c2c1c84d8f9fe1c690e99ef7f83507183ae60
Yang Seok Jin <tjrwls08088@naver.com>
2024-12-11 20:50:53 +0900

    chore: .env 환경 변수 세팅

--- peauty-customer-api/src/main/java/com/peauty/customer/presentation/config/PaymentConfig.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/config/PaymentConfig.java
@@ -2,16 +2,22 @@ package com.peauty.customer.presentation.config;
 
 
 import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // TODO: payment 패키지에서 관리할 수 있도록 리팩토링
 @Configuration
 public class PaymentConfig {
+
+    @Value("${iamport.api-key}")
+    private String apiKey;
+
+    @Value("${iamport.api-secret-key}")
+    private String apiSecretKey;
+
     @Bean
     public IamportClient iamportClient() {
-        String apiKey = "${IAMPORT_API_KEY}";
-        String apiSecret = "${IAMPORT_API_SECRET_KEY}";
-        return new IamportClient(apiKey, apiSecret);
+        return new IamportClient(apiKey, apiSecretKey);
     }
 }
