c05a505cd671ca1cbc95eea3c30f380a12f403af
Yang Seok Jin <tjrwls08088@naver.com>
2024-12-11 22:18:14 +0900

    style: 요구사항 변경으로 사용하지 못하는 유효성 검사 주석

--- peauty-domain/src/main/java/com/peauty/domain/payment/Payment.java
+++ peauty-domain/src/main/java/com/peauty/domain/payment/Payment.java
@@ -69,6 +69,10 @@ public class Payment {
         if(actualPrice == null || estimatePrice == null) {
             throw new PeautyException(PeautyResponseCode.NOT_FOUND_ACTUAL_PRICE);
         }
+
+        // TODO. DB에 저장한 견적서는 예약금이 아니지만 클라이언트에서 받는 값은 예약금 금액을 받기 때문에
+        //       해당 유효성 검사가 정상적으로 작동하지 않는다. 해당 검사는 클라이언트에서 예약금이 아닌 실제 결제 금액
+        //       을 가져올 때 사용 가능하다.
 //        else if(estimatePrice != actualPrice) {
 //            throw new PeautyException(PeautyResponseCode.PAYMENT_AMOUNT_NOT_EQUALS);
 //        }
