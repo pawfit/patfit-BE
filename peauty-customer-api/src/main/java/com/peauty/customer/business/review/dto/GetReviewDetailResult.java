acc8981cc08fdf94d909a898bc2ed23814e751a0
Shanate <redeyes5240@gmail.com>
2024-12-12 19:55:19 +0900

    feat: Impl Get ReviewImages

--- peauty-customer-api/src/main/java/com/peauty/customer/business/review/dto/GetReviewDetailResult.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/business/review/dto/GetReviewDetailResult.java
@@ -3,14 +3,18 @@ package com.peauty.customer.business.review.dto;
 import com.peauty.domain.bidding.BiddingThread;
 import com.peauty.domain.review.ContentGeneral;
 import com.peauty.domain.review.Review;
+import com.peauty.domain.review.ReviewImage;
 import com.peauty.domain.review.ReviewRating;
+
+import java.util.List;
 
 public record GetReviewDetailResult(
         Review.ID reviewId,
         BiddingThread.ID biddingThreadId,
         ReviewRating reviewRating,
         String contentDetail,
-        ContentGeneral contentGeneral
+        ContentGeneral contentGeneral,
+        List<String> reviewImages
 ) {
     public static GetReviewDetailResult from(Review review) {
         return new GetReviewDetailResult(
@@ -18,7 +22,8 @@ public record GetReviewDetailResult(
                 review.getThreadId(),
                 review.getReviewRating(),
                 review.getContentDetail(),
-                review.getContentGeneral()
+                review.getContentGeneral(),
+                review.getReviewImages().stream().map(ReviewImage::getImageUrl).toList()
         );
     }
 }
--- peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/review/dto/GetReviewDetailResponse.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/review/dto/GetReviewDetailResponse.java
@@ -2,12 +2,15 @@ package com.peauty.customer.presentation.controller.review.dto;
 
 import com.peauty.customer.business.review.dto.GetReviewDetailResult;
 
+import java.util.List;
+
 public record GetReviewDetailResponse(
         Long reviewId,
         Long biddingThreadId,
         String reviewRating,
         String contentDetail,
-        String contentGeneral
+        String contentGeneral,
+        List<String> reviewImages
 ) {
 
     public static GetReviewDetailResponse from(GetReviewDetailResult result){
@@ -16,7 +19,8 @@ public record GetReviewDetailResponse(
                 result.biddingThreadId().value(),
                 result.reviewRating().name(),
                 result.contentDetail(),
-                result.contentGeneral().name()
+                result.contentGeneral().name(),
+                result.reviewImages()
         );
     }
 }
