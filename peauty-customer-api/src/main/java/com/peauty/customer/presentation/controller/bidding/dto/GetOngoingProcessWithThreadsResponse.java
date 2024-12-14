26045af6d8239480e95705bbe2a395db98df5747
wewee <90890216+weejinyoung@users.noreply.github.com>
2024-12-14 11:09:16 +0900

    [PEAUTY-244] Impl bidding system select api for ui (#89)
    * feat: impl findOngoingProcessByPuppyId
    
    * feat: add getProfile with EstimateProposal in process
    
    * fix: getProcessByPuppyId -> getProcessesByPuppyId in designer api
    
    * feat: bidding system select api for ui
    
    * feat: dto for api
    
    * refact: post -> patch req for mj

---
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/bidding/dto/GetOngoingProcessWithThreadsResponse.java
@@ -0,0 +1,20 @@
+package com.peauty.customer.presentation.controller.bidding.dto;
+
+import com.peauty.customer.business.bidding.dto.GetOngoingProcessWithThreadsResult;
+import com.peauty.domain.bidding.BiddingProcess;
+import com.peauty.domain.bidding.BiddingThread;
+
+import java.util.List;
+
+public record GetOngoingProcessWithThreadsResponse(
+        BiddingProcess.Profile process,
+        List<BiddingThread.Profile> threads
+) {
+
+    public static GetOngoingProcessWithThreadsResponse from(GetOngoingProcessWithThreadsResult result) {
+        if (result == null) {
+            return new GetOngoingProcessWithThreadsResponse(null, null);
+        }
+        return new GetOngoingProcessWithThreadsResponse(result.process(), result.threads());
+    }
+}
--- peauty-designer-api/src/main/java/com/peauty/designer/business/bidding/BiddingProcessPort.java
+++ peauty-designer-api/src/main/java/com/peauty/designer/business/bidding/BiddingProcessPort.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface BiddingProcessPort {
 
     BiddingProcess getProcessByProcessId(Long processId);
-    BiddingProcess getProcessByPuppyId(Long puppyId);
+    List<BiddingProcess> getProcessesByPuppyId(Long puppyId);
     BiddingProcess getProcessByProcessIdAndPuppyId(Long processId, Long puppyId);
     BiddingProcess save(BiddingProcess process);
     List<BiddingProcess> getProcessesByDesignerId(Long designerId);
