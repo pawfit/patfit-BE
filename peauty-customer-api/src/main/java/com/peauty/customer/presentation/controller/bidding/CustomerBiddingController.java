34c1d78d20bf237bff2a95f83f71e05590ddd69f
weejinyoung <a8118199@gmail.com>
2024-12-14 14:11:16 +0900

    refact: url and swagger desc update

--- peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/bidding/CustomerBiddingController.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/bidding/CustomerBiddingController.java
@@ -16,15 +16,15 @@ public class CustomerBiddingController {
 
     private final CustomerBiddingService customerBiddingService;
 
-    @GetMapping("/{userId}/puppies/")
-    @Operation(summary = "입찰 프로세스 시작을 위한 강아지 프로필 조히", description = "프로세스 진행 여부가 포함된 등록된 강아지 프로필 조회")
+    @GetMapping("/{userId}/puppies/with-bidding-available")
+    @Operation(summary = "입찰 프로세스 시작을 위한 강아지 조회", description = "진행 중인 프로세스 여부가 포함된 강아지 조회.")
     public GetPuppyProfilesWithCanStartProcessStatusResponse getPuppyProfilesWithCanStartProcessStatus(@PathVariable Long userId) {
         GetPuppyProfilesWithCanStartProcessStatusResult result = customerBiddingService.getPuppyProfilesWithCanStartProcessStatus(userId);
         return GetPuppyProfilesWithCanStartProcessStatusResponse.from(result);
     }
 
     @PostMapping("/{userId}/puppies/{puppyId}/bidding/processes")
-    @Operation(summary = "견적요청서 보내기", description = "선택한 디자이너들과 스레드를 시작함과 함께 프로세스를 시작")
+    @Operation(summary = "견적요청서 보내기", description = "선택한 디자이너들과 스레드를 시작함과 함께 프로세스를 시작.")
     public SendEstimateProposalResponse initProcessWithSendEstimateProposal(
             @PathVariable Long userId,
             @PathVariable Long puppyId,
