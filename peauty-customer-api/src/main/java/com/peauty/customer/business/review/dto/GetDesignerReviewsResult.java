b237c9b5e2833426dd65b575712472dee10ac21b
Shanate <redeyes5240@gmail.com>
2024-12-15 15:50:34 +0900

    feat: Impl DesignerReviews

---
+++ peauty-customer-api/src/main/java/com/peauty/customer/business/review/dto/GetDesignerReviewsResult.java
@@ -0,0 +1,19 @@
+package com.peauty.customer.business.review.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record GetDesignerReviewsResult(
+        Long designerId,
+        List<ReviewDetails> reviews
+) {
+    public record ReviewDetails(
+            LocalDate reviewDate,
+            String customerNickname,
+            String totalGroomingBodyType,
+            String totalGroomingFaceType,
+            Double rating,
+            List<String> images,
+            String content
+    ) {}
+}
---
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/review/dto/GetDesignerReviewsResponse.java
@@ -0,0 +1,38 @@
+package com.peauty.customer.presentation.controller.review.dto;
+
+import com.peauty.customer.business.review.dto.GetDesignerReviewsResult;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record GetDesignerReviewsResponse(
+        Long designerId,
+        List<ReviewDetails> reviews
+) {
+    public record ReviewDetails(
+            LocalDate reviewDate,
+            String customerNickname,
+            String totalGroomingBodyType,
+            String totalGroomingFaceType,
+            Double rating,
+            List<String> images,
+            String content
+    ) {}
+
+    public static GetDesignerReviewsResponse from(GetDesignerReviewsResult result) {
+        return new GetDesignerReviewsResponse(
+                result.designerId(),
+                result.reviews().stream()
+                        .map(review -> new ReviewDetails(
+                                review.reviewDate(),
+                                review.customerNickname(),
+                                review.totalGroomingBodyType(),
+                                review.totalGroomingFaceType(),
+                                review.rating(),
+                                review.images(),
+                                review.content()
+                        ))
+                        .toList()
+        );
+    }
+}
