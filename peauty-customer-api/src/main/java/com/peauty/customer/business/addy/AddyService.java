b95cbc7bde31fffe450408e442cf11bddfbbf00e
Yang Seok Jin <tjrwls08088@naver.com>
2024-12-15 17:06:33 +0900

    docs: import 구문 정리

--- peauty-customer-api/src/main/java/com/peauty/customer/business/addy/AddyService.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/business/addy/AddyService.java
@@ -2,10 +2,6 @@ package com.peauty.customer.business.addy;
 
 import com.peauty.customer.business.addy.dto.CreateAddyImageCommand;
 import com.peauty.customer.business.addy.dto.CreateAddyImageResult;
-import com.peauty.customer.presentation.controller.Addy.dto.CreateAddyImageRequest;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.awt.*;
 
 public interface AddyService {
     CreateAddyImageResult createAddyImage(
