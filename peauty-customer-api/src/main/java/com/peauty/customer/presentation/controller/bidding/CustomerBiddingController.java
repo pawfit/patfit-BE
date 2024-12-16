dc855bca7fe5ff1ce1d8ccd1f210c230357dd06b
weejinyoung <a8118199@gmail.com>
2024-12-16 12:39:48 +0900

    fix: rollback comment

--- peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/bidding/CustomerBiddingController.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/bidding/CustomerBiddingController.java
@@ -105,21 +105,20 @@ public class CustomerBiddingController {
         return GetEstimateProposalDetailResponse.from(result);
     }
 
-    // TODO 변경사항 적용 확인 시 돌려놓기
-//    @GetMapping("/{userId}/puppies/{puppyId}/bidding/processes/{processId}/threads/{threadId}")
-//    @Operation(summary = "견적요청서 & 견적서 상세정보 조회", description = "해당하는 프로세스, 스레드와 연관된 견적요청서와 견적서의 상세정보를 조회.")
-//    public GetEstimateAndProposalDetailsResponse getEstimateAndProposalDetails(
-//            @PathVariable Long userId,
-//            @PathVariable Long puppyId,
-//            @PathVariable Long processId,
-//            @PathVariable Long threadId
-//    ) {
-//        GetEstimateAndProposalDetailsResult result = customerBiddingService.getEstimateAndProposalDetails(
-//                userId,
-//                puppyId,
-//                processId,
-//                threadId
-//        );
-//        return GetEstimateAndProposalDetailsResponse.from(result);
-//    }
+    @GetMapping("/{userId}/puppies/{puppyId}/bidding/processes/{processId}/threads/{threadId}")
+    @Operation(summary = "견적요청서 & 견적서 상세정보 조회", description = "해당하는 프로세스, 스레드와 연관된 견적요청서와 견적서의 상세정보를 조회.")
+    public GetEstimateAndProposalDetailsResponse getEstimateAndProposalDetails(
+            @PathVariable Long userId,
+            @PathVariable Long puppyId,
+            @PathVariable Long processId,
+            @PathVariable Long threadId
+    ) {
+        GetEstimateAndProposalDetailsResult result = customerBiddingService.getEstimateAndProposalDetails(
+                userId,
+                puppyId,
+                processId,
+                threadId
+        );
+        return GetEstimateAndProposalDetailsResponse.from(result);
+    }
 }
