fda143833ee0435cd5b54391cd10181d55b053c7
Shanate <redeyes5240@gmail.com>
2024-12-17 21:36:05 +0900

    feat: Swagger Docs about Review

--- peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/review/dto/RegisterReviewResponse.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/review/dto/RegisterReviewResponse.java
@@ -1,14 +1,20 @@
 package com.peauty.customer.presentation.controller.review.dto;
 
 import com.peauty.customer.business.review.dto.RegisterReviewResult;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record RegisterReviewResponse(
+        @Schema(description = "리뷰 ID(PK)", example = "1")
         Long reviewId,
+        @Schema(description = "비딩 스레드 ID(FK)", example = "1")
         Long biddingThreadId,
+        @Schema(description = "리뷰 평점", example = "TWO")
         Double reviewRating,
+        @Schema(description = "리뷰 내용", example = "저희 개가 아주 이뻐졌어요. 그런데 리트리버였는데 왜 닥스훈트가 됐을까요")
         String contentDetail,
+        @Schema(description = "선택 리뷰", example = "[\"GOOD_SERVICE\", \"COME_AGAIN\"]")
         List<String> contentGenerals
 ) {
     public static RegisterReviewResponse from(RegisterReviewResult result) {
--- peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/review/dto/UpdateReviewRequest.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/presentation/controller/review/dto/UpdateReviewRequest.java
@@ -4,13 +4,18 @@ import com.peauty.customer.business.review.dto.UpdateReviewCommand;
 import com.peauty.domain.review.ContentGeneral;
 import com.peauty.domain.review.ReviewImage;
 import com.peauty.domain.review.ReviewRating;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
 public record UpdateReviewRequest(
+        @Schema(description = "리뷰 평점", example = "FOUR")
         ReviewRating reviewRating,
+        @Schema(description = "리뷰 내용", example = "뽀송뽀송해진 우리 개 어때영")
         String contentDetail,
+        @Schema(description = "선택 리뷰", example = "KIND")
         List<ContentGeneral> contentGenerals,
+        @Schema(description = "사진", example = "[\"https://peauty.s3.ap-northeast-2.amazonaws.com/images/b24c84d4-0295-43e0-bdb5-238889f3b5c8.jpg\"]")
         List<String> reviewImageUrls
 ) {
 
