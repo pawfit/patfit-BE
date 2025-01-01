c7087e0d87ff1443956245c6b67c7ec2cb587566
wewee <90890216+weejinyoung@users.noreply.github.com>
2025-01-02 00:18:51 +0900

    [HANSEL-118] Set querydsl and test setting (#119)
    * env: querydsl setting
    
    * test: querydsl setting test

--- peauty-customer-api/src/main/java/com/peauty/customer/PeautyCustomerApplication.java
+++ peauty-customer-api/src/main/java/com/peauty/customer/PeautyCustomerApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan("com.peauty.persistence")
-@EnableJpaRepositories("com.peauty.persistence")
 @SpringBootApplication(
         scanBasePackages = {
                 "com.peauty.domain",
--- peauty-persistence/src/main/java/com/peauty/persistence/config/JpaConfig.java
+++ peauty-persistence/src/main/java/com/peauty/persistence/config/JpaConfig.java
@@ -1,11 +1,16 @@
 package com.peauty.persistence.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaAuditing
 @EnableTransactionManagement
+@EntityScan(basePackages = "com.peauty.persistence")
+@EnableJpaRepositories(basePackages = "com.peauty.persistence")
 public class JpaConfig {
 }
