9664ecb0a4081c1aef54610c46f10bc71a917183
weejinyoung <a8118199@gmail.com>
2025-01-01 20:04:33 +0900

    env: querydsl setting

--- peauty-designer-api/src/main/java/com/peauty/designer/PeautyDesignerApplication.java
+++ peauty-designer-api/src/main/java/com/peauty/designer/PeautyDesignerApplication.java
@@ -5,23 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan("com.peauty.persistence")
-@EnableJpaRepositories("com.peauty.persistence")
 @SpringBootApplication(
-		scanBasePackages = {
-				"com.peauty.domain",
-				"com.peauty.logging",
-				"com.peauty.persistence",
-				"com.peauty.auth",
-				"com.peauty.cache",
-				"com.peauty.aws",
-				"com.peauty.designer"
-		}
+        scanBasePackages = {
+                "com.peauty.domain",
+                "com.peauty.logging",
+                "com.peauty.persistence",
+                "com.peauty.auth",
+                "com.peauty.cache",
+                "com.peauty.aws",
+                "com.peauty.designer"
+        }
 )
 public class PeautyDesignerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PeautyDesignerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PeautyDesignerApplication.class, args);
+    }
 
 }
