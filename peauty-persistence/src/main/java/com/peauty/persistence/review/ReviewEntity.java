0040211a34657088feb4479a1f740714eb7b28a5
Shanate <redeyes5240@gmail.com>
2025-01-14 20:06:11 +0900

    feat: Add Column Version in WorkspaceEntity

--- peauty-persistence/src/main/java/com/peauty/persistence/review/ReviewEntity.java
+++ peauty-persistence/src/main/java/com/peauty/persistence/review/ReviewEntity.java
@@ -20,9 +20,6 @@ public class ReviewEntity extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Version
-    private Integer version;
-
     @Column(name = "bidding_thread_id", nullable = false)
     private Long biddingThreadId;
 
